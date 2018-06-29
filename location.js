45d9692cba56e35299fbe688230e9588cbe6b877
AntonioPintor <antonyopintor@gmail.com>
2018-06-29 15:45:09 -0300

    Localizar Cidade ou Estado
    Localizar Cidade ou Estado

---
+++ location.js
@@ -0,0 +1,9 @@
+var LIP_LowPrecision=true;function LocalizaIP_done(a){if(!a.error)
+document.getElementById("pa").innerHTML =(' '+ip_data['city']+'');
+window.location.href =('#'+ip_data['city']+'-'+ip_data['state']+'-'+ip_data['country']+'');
+document.getElementById("pf").innerHTML =(' '+ip_data['city']+' - '+ip_data['state']+'');
+document.getElementById("p0").innerHTML =(' '+ip_data['city']+' - '+ip_data['state']+'');
+document.getElementById("p1").innerHTML =(' '+ip_data['city']+' - '+ip_data['state']+'');  
+document.getElementById("p2").innerHTML =(' '+ip_data['city']+' - '+ip_data['state']+''); 
+document.getElementById("p3").innerHTML =(' '+ip_data['city']+' - '+ip_data['state']+'');
+}
